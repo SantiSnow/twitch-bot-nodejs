cf70c9a3d9cc17707791e0ed6a5e4e0dccacd5e3
Santiago Aguirre <aguirresantiago81@gmail.com>
2021-01-15 17:22:24 -0300

    Agregado nodemon al proyecto, agregado un saludo por parte del bot

--- src/bot.js
+++ src/bot.js
@@ -5,7 +5,14 @@ const cliente = new tmi.client(opciones.opciones);
 
 cliente.connect();
 
+//eventos
 cliente.on('connected', (address, port)=>{
     cliente.action('iamacat95',
         `¡Hola, soy el Santi-bot y estoy listo para trabajar!`)
+});
+
+cliente.on('chat', (target, ctx, message, self)=>{
+    if(self) return
+
+    cliente.say(target, `Bienvenido al chat ${ctx.username}`);
 });
